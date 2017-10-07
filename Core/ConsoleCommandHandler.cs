719eafee87a95ef2031a6c5dffe4582968075a01
slaapkopamy <32595704+slaapkopamy@users.noreply.github.com>
2017-10-07 16:40:21 +0200

    Add files via upload

---
+++ Core/ConsoleCommandHandler.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using log4net;
+using Quasar.HabboHotel;
+using Quasar.HabboHotel.Global;
+
+using Quasar.Communication.Packets.Outgoing.Moderation;
+
+namespace Quasar.Core
+{
+    public class ConsoleCommandHandler
+    {
+        private static readonly ILog log = LogManager.GetLogger("Quasar.Core.ConsoleCommandHandler");
+
+        public static object LanguageLocal { get; private set; }
+
+        public static void InvokeCommand(string inputData)
+        {
+            if (string.IsNullOrEmpty(inputData))
+                return;
+
+            try
+            {
+                #region Command parsing
+                string CurrentTime = DateTime.Now.ToString("HH:mm:ss" + " | ");
+                string[] parameters = inputData.Split(' ');
+
+                switch (parameters[0].ToLower())
+                {
+                    #region stop
+                    case "stop":
+                    case "shutdown":
+                        {
+                            Logging.DisablePrimaryWriting(true);
+                            Logging.WriteLine("The server is saving users furniture, rooms, etc. WAIT FOR THE SERVER TO CLOSE, DO NOT EXIT THE PROCESS IN TASK MANAGER!!", ConsoleColor.Yellow);
+                            QuasarEnvironment.PerformShutDown();
+                            break;
+                        }
+                    #endregion
+
+                    #region alert
+                    case "alert":
+                        {
+                            string Notice = inputData.Substring(6);
+
+                            QuasarEnvironment.GetGame().GetClientManager().SendMessage(new BroadcastMessageAlertComposer(Notice));
+
+                            Console.WriteLine(CurrentTime + "» Alert met succes verzonden");
+                            break;
+                        }
+                    #endregion
+
+                    default:
+                        {
+                            Console.WriteLine(CurrentTime + "» " + parameters[0].ToLower() + " is een niet bestaande functie - toets :help in voor meer informatie.");
+                            break;
+                        }
+                }
+                #endregion
+            }
+            catch (Exception e)
+            {
+                string CurrentTime1 = DateTime.Now.ToString("HH:mm:ss" + " | ");
+                Console.WriteLine(CurrentTime1 + "» Fout in command [" + inputData + "]: " + e);
+            }
+        }
+    }
+}
---
+++ Core/Logging.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Text;
+using ConsoleWriter;
+
+namespace Quasar.Core
+{
+    public static class Logging
+     {
+        public static bool DisabledState
+        {
+            get { return Writer.DisabledState; }
+            set { Writer.DisabledState = value; }
+        }
+
+        public static void WriteLine(string Line, ConsoleColor Colour = ConsoleColor.Gray)
+        {
+            Writer.WriteLine(Line, Colour);
+        }
+
+        public static void LogException(string logText)
+        {
+            string CurrentTime = DateTime.Now.ToString("HH:mm:ss" + " | ");
+            Writer.LogException(CurrentTime + Environment.NewLine + logText + Environment.NewLine);
+        }
+
+        public static void LogMySQLError(string logText)
+        {
+            string CurrentTime = DateTime.Now.ToString("HH:mm:ss" + " | ");
+            Writer.LogMySQLError(CurrentTime + Environment.NewLine + logText + Environment.NewLine);
+        }
+
+        public static void LogWiredException(string logText)
+        {
+            string CurrentTime = DateTime.Now.ToString("HH:mm:ss" + " | ");
+            Writer.LogWiredException(CurrentTime + logText);
+        }
+
+        public static void LogCacheException(string logText)
+        {
+            Writer.LogCacheException(logText);
+        }
+
+        public static void LogPathfinderException(string logText)
+        {
+            string CurrentTime = DateTime.Now.ToString("HH:mm:ss" + " | ");
+            Writer.LogPathfinderException(CurrentTime + logText);
+        }
+
+        public static void LogCriticalException(string logText)
+        {
+            string CurrentTime = DateTime.Now.ToString("HH:mm:ss" + " | ");
+            Writer.LogCriticalException(CurrentTime + logText);
+        }
+
+        public static void LogThreadException(string Exception, string Threadname)
+        {
+            string CurrentTime = DateTime.Now.ToString("HH:mm:ss" + " | ");
+            Writer.LogThreadException(CurrentTime + Exception, Threadname);
+        }
+
+        public static void LogPacketException(string Packet, string Exception)
+        {
+            string CurrentTime = DateTime.Now.ToString("HH:mm:ss" + " | ");
+            Writer.LogPacketException(CurrentTime + Packet, Exception);
+        }
+
+        public static void HandleException(Exception pException, string pLocation)
+        {
+            Writer.HandleException(pException, pLocation);
+        }
+
+        public static void DisablePrimaryWriting(bool ClearConsole)
+        {
+            Writer.DisablePrimaryWriting(ClearConsole);
+        }
+    }
+}
