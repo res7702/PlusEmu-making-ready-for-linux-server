e0180210e5a61c3dfe474716e8dae9959c17ee31
slaapkopamy <32595704+slaapkopamy@users.noreply.github.com>
2017-10-08 21:53:58 +0200

    Add files via upload

---
+++ HabboHotel/Rooms/Chat/Styles/ChatStyleManager.cs
@@ -0,0 +1,60 @@
+﻿using log4net;
+using Quasar.Database.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quasar.HabboHotel.Rooms.Chat.Styles
+{
+    public sealed class ChatStyleManager
+    {
+        private static readonly ILog log = LogManager.GetLogger("Quasar.HabboHotel.Rooms.Chat.Styles.ChatStyleManager");
+
+        private readonly Dictionary<int, ChatStyle> _styles;
+
+        public ChatStyleManager()
+        {
+            this._styles = new Dictionary<int, ChatStyle>();
+        }
+
+        public void Init()
+        {
+            if (this._styles.Count > 0)
+                this._styles.Clear();
+            string CurrentTime = DateTime.Now.ToString("HH:mm:ss" + " | ");
+            DataTable Table = null;
+            using (IQueryAdapter dbClient = QuasarEnvironment.GetDatabaseManager().GetQueryReactor())
+            {
+                dbClient.SetQuery("SELECT * FROM `room_chat_styles`;");
+                Table = dbClient.getTable();
+
+                if (Table != null)
+                {
+                    foreach (DataRow Row in Table.Rows)
+                    {
+                        try
+                        {
+                            if (!this._styles.ContainsKey(Convert.ToInt32(Row["id"])))
+                                this._styles.Add(Convert.ToInt32(Row["id"]), new ChatStyle(Convert.ToInt32(Row["id"]), Convert.ToString(Row["name"]), Convert.ToString(Row["required_right"])));
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(CurrentTime + "» Kan chatbubble [" + Convert.ToInt32(Row["id"]) + "] moet laden", ex);
+                        }
+                    }
+                }
+            }
+
+            
+            Console.WriteLine(CurrentTime + "» Chat " + this._styles.Count + " bubbels");
+        }
+
+        public bool TryGetStyle(int Id, out ChatStyle Style)
+        {
+            return this._styles.TryGetValue(Id, out Style);
+        }
+    }
+}
